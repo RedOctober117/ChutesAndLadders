373296bc0e2cab0987b89f5c5f4d5dd714253ddf
RedOctober117 <dbcahilly3@gmail.com>
2023-03-29 14:31:58 -0400

    cleaned Board.java

--- Board.java
+++ Board.java
@@ -1,30 +1,7 @@
 public class Board{
-  public static final char BORDER = '*';
   private int playerCount;
   private int dimension;
 
-/*  public Board(int dimension, char initial, int playerX, int playerY){
-    this.dimension = dimension;
-    for (int k = 0; k < dimension; k++){
-      for (int j = 0; j < (6 * dimension) + 1; j++){
-        System.out.print("*");
-      }         
-        System.out.print("\n");
-        for (int k = 0; k < 61; k++){
-           if (k == 60){
-             System.out.print("*\n");
-          } else {
-            if(playerX == j && playerY = i)
-             System.out.printf(initial); 
-          }
-        }
-        System.out.print("*\n");
-      }
-    for (int j = 0; j < (6 * dimension) + 1; j++){
-      System.out.print("*");
-    }
-  }
-*/
 public Board(int dimension, char initial, int playerX, int playerY){
   this.dimension = dimension;
   int a = 0;
@@ -50,34 +27,9 @@ public Board(int dimension, char initial, int playerX, int playerY){
         }
       }
     }
-        System.out.print("\n");
+    System.out.print("\n");
   }
 }
-/*
-  public Board(int dimension, int playerCount){
-    this.dimension = dimension;
-    this.playerCount = playerCount;
-    for (int i = 0; i < dimension; i++){
-      for (int j = 0; j < (6 * dimension) + 1; j++){
-        System.out.print("*");
-      }         
-        System.out.print("\n");
-        for (int k = 0; k < 23; k++){
-           if (k == dimension + 1){
-             System.out.print("\n");
-          } else {
-             System.out.print("*     ");
-          }
-        }
-        System.out.print("\n");
-      }
-    for (int j = 0; j < (6 * dimension) + 1; j++){
-      System.out.print("*");
-    }
-  }
-*/
-
-
 
 /********************************
  *                              *
--- Token.java
+++ Token.java
@@ -1,3 +1,4 @@
+
 public class Token{
   private int x;
   private int y;
