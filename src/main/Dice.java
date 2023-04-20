80e9438116c5a1f47b5917f89d88bc39519cbb7e
RedOctober117 <dbcahilly3@gmail.com>
2023-04-20 10:17:19 -0400

    Dice update

--- src/main/Dice.java
+++ src/main/Dice.java
@@ -1,87 +1,80 @@
-import processing.core.*;
-import java.util.Random;
+import processing.core.PApplet;
 
 public class Dice extends PApplet {
 
-  private int diceValue = 1;
-  private int cycleCount = 0;
-  private Random rand = new Random();
+    private int diceValue = 1;
+    private int cycleCount = 0;
 
-  public void settings() {
-    size(200, 200);
-  }
-
-  public void setup() {
-    textAlign(CENTER);
-    textSize(32);
-  }
-
-  public void draw() {
-    frameRate(30);
-    background(255);
-    fill(0);
-    rect(50, 50, 100, 100);
-    fill(255);
-    switch(diceValue) {
-      case 1:
-        ellipse(100, 100, 20, 20);
-        break;
-      case 2:
-        ellipse(75, 75, 20, 20);
-        ellipse(125, 125, 20, 20);
-        break;
-      case 3:
-        ellipse(75, 75, 20, 20);
-        ellipse(100, 100, 20, 20);
-        ellipse(125, 125, 20, 20);
-        break;
-      case 4:
-        ellipse(75, 75, 20, 20);
-        ellipse(125, 75, 20, 20);
-        ellipse(75, 125, 20, 20);
-        ellipse(125, 125, 20, 20);
-        break;
-      case 5:
-        ellipse(75, 75, 20, 20);
-        ellipse(125, 75, 20, 20);
-        ellipse(100, 100, 20, 20);
-        ellipse(75, 125, 20, 20);
-        ellipse(125, 125, 20, 20);
-        break;
-      case 6:
-        ellipse(75, 75, 20, 20);
-        ellipse(125, 75, 20, 20);
-        ellipse(75, 100, 20, 20);
-        ellipse(125, 100, 20, 20);
-        ellipse(75, 125, 20, 20);
-        ellipse(125, 125, 20, 20);
-        break;
+    public void settings() {
+        size(200, 200);
     }
 
-    if (cycleCount < 6) {
-      if (frameCount % 10 == 0) {
-        diceValue = rand.nextInt(6) + 1;
-        cycleCount++;
-      }
-    } else {
-      textSize(20);
-      fill(0);
-      text("Your Roll: " + diceValue, width/2, height/5);
-      thread("closeWindow");
+    public void setup() {
+        textAlign(CENTER);
+        textSize(32);
     }
-  }
 
-  public void closeWindow() {
-    delay(1000); // wait for 1 second
-    exit();
-  }
+    public void draw() {
+        frameRate(30);
+        background(255);
+        fill(0);
+        rect(50, 50, 100, 100);
+        fill(255);
+        switch (diceValue) {
+            case 1:
+                ellipse(100, 100, 20, 20);
+                break;
+            case 2:
+                ellipse(75, 75, 20, 20);
+                ellipse(125, 125, 20, 20);
+                break;
+            case 3:
+                ellipse(75, 75, 20, 20);
+                ellipse(100, 100, 20, 20);
+                ellipse(125, 125, 20, 20);
+                break;
+            case 4:
+                ellipse(75, 75, 20, 20);
+                ellipse(125, 75, 20, 20);
+                ellipse(75, 125, 20, 20);
+                ellipse(125, 125, 20, 20);
+                break;
+            case 5:
+                ellipse(75, 75, 20, 20);
+                ellipse(125, 75, 20, 20);
+                ellipse(100, 100, 20, 20);
+                ellipse(75, 125, 20, 20);
+                ellipse(125, 125, 20, 20);
+                break;
+            case 6:
+                ellipse(75, 75, 20, 20);
+                ellipse(125, 75, 20, 20);
+                ellipse(75, 100, 20, 20);
+                ellipse(125, 100, 20, 20);
+                ellipse(75, 125, 20, 20);
+                ellipse(125, 125, 20, 20);
+                break;
+        }
 
-  // New public method to return the diceValue variable
-  public int getDiceValue() {
-    return diceValue;
-  }
+        if (cycleCount < 6) {
+            if (frameCount % 10 == 0) {
+                diceValue = Main.rollDice();
+                cycleCount++;
+            }
+        } else {
+            textSize(20);
+            fill(0);
+            text("Your Roll: " + diceValue, width / 2, height / 5);
+            thread("closeWindow");
+        }
+    }
 
-  public static void main(String[] args) {
-    PApplet.main("Dice");
-  }
+    public void closeWindow() {
+        delay(1000); // wait for 1 second
+        exit();
+    }
+
+    public static void main(String[] args) {
+        PApplet.main("Dice");
+    }
 }
